5189e1c27c06d1f23b35c2c59fd44ce2a3fe0e7e
holliwid <stepales1111@gmail.com>
2021-03-05 09:58:21 +0300

    Add excel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import openpyxl
 
 URL = 'https://studynow.ru/dicta/allwords'
 page = requests.get(URL)
@@ -19,9 +20,9 @@ rus_words = []
 flag = 0
 for word in list_of_raw_data:
     if flag % 3 == 1:
-        eng_words.append(word)
+        eng_words.append(word.split(',')[0])
     if flag % 3 == 2:
-        rus_words.append(word)
+        rus_words.append(word.split(',')[0].split('(')[0])
     flag += 1
     if flag == 301:
         break
@@ -33,3 +34,11 @@ for x in rus_words:
     print(x)
 
 
+wb = openpyxl.load_workbook(filename=r'C:\Users\markovDD\OneDrive\Рабочий стол\Офисы\Dictionary.xlsx')
+ws = wb.worksheets[1]
+for index in range(100):
+    ws.cell(row=index+7, column=3).value = rus_words[index]
+    ws.cell(row=index+7, column=12).value = rus_words[index]
+    ws.cell(row=index+7, column=4).value = eng_words[index]
+    ws.cell(row=index+7, column=11).value = eng_words[index]
+wb.save('Dictionary.xlsx')
